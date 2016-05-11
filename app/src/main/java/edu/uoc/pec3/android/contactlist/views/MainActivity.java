4eb01199017ded89e85327d2a3a5bd8b8ee58adf
Marc Callís Vilalta <callis.marc@gmail.com>
2016-05-11 23:31:59 +0200

    Define RecyclerView and adapter

--- app/src/main/java/edu/uoc/pec3/android/contactlist/views/MainActivity.java
+++ app/src/main/java/edu/uoc/pec3/android/contactlist/views/MainActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import edu.uoc.pec3.android.contactlist.R;
+import edu.uoc.pec3.android.contactlist.adapters.ContactsArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecycler;
+    private ContactsArrayAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        
 
     }
 
