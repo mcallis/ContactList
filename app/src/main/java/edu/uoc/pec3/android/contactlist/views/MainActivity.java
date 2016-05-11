096ef24a758b5f45caf520e91aed7e29c2fed999
Marc Callís Vilalta <callis.marc@gmail.com>
2016-05-11 23:13:05 +0200

    All gradle properties

---
+++ app/src/main/java/edu/uoc/pec3/android/contactlist/views/MainActivity.java
@@ -0,0 +1,23 @@
+package edu.uoc.pec3.android.contactlist.views;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import edu.uoc.pec3.android.contactlist.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
---
+++ app/src/main/java/edu/uoc/pec3/android/contactlist/views/SplashActivity.java
@@ -0,0 +1,56 @@
+package edu.uoc.pec3.android.contactlist.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import edu.uoc.pec3.android.contactlist.R;
+import edu.uoc.pec3.android.contactlist.manager.FirebaseContactManager;
+import edu.uoc.pec3.android.contactlist.model.Contact;
+
+/**
+ * Created by mgarcia on 24/03/2016.
+ */
+public class SplashActivity extends AppCompatActivity implements ValueEventListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseContactManager.getInstance().getContactFromServer(this);
+    }
+
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        for (DataSnapshot contact: dataSnapshot.getChildren()) {
+            FirebaseContactManager.getInstance().addContactHashMap(contact.getValue(Contact.class));
+        }
+        startMainActivity();
+    }
+
+    @Override
+    public void onCancelled(FirebaseError firebaseError) {
+        startMainActivity();
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
