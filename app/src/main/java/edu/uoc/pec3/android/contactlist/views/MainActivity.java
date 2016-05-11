ce3f5375968b788de18cb476fa4fdb7ed7c8891e
Marc Callís Vilalta <callis.marc@gmail.com>
2016-05-12 00:34:37 +0200

    MainActivity config

--- app/src/main/java/edu/uoc/pec3/android/contactlist/views/MainActivity.java
+++ app/src/main/java/edu/uoc/pec3/android/contactlist/views/MainActivity.java
@@ -1,20 +1,30 @@
 package edu.uoc.pec3.android.contactlist.views;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.List;
+
 import edu.uoc.pec3.android.contactlist.R;
 import edu.uoc.pec3.android.contactlist.adapters.ContactsArrayAdapter;
+import edu.uoc.pec3.android.contactlist.manager.FirebaseContactManager;
+import edu.uoc.pec3.android.contactlist.model.Contact;
+import edu.uoc.pec3.android.contactlist.utils.RecyclerItemClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private List<Contact> mListContact;
     private RecyclerView mRecycler;
     private ContactsArrayAdapter mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +33,47 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        
+        mRecycler = (RecyclerView)findViewById(R.id.recycler);
+        mRecycler.setHasFixedSize(true);
+        // Connect the RecyclerView to controller
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        mRecycler.setLayoutManager(llm);
+        // Detecting gesture
+        mRecycler.addOnItemTouchListener(onRecyclerItemClickListener());
 
+        mListContact = FirebaseContactManager.getInstance().getAllContacts();
+
+        if (mListContact == null){
+            showAlert("Contact information is not available");
+        } else {
+            mAdapter = new ContactsArrayAdapter(this, mListContact);
+            mRecycler.setAdapter(mAdapter);
+        }
+
+    }
+
+    private RecyclerItemClickListener onRecyclerItemClickListener(){
+        return new RecyclerItemClickListener(MainActivity.this, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Contact currentContact = mListContact.get(position);
+                showDetail(currentContact.getObjectId());
+            }
+        });
+    }
+
+    private void showDetail(String contactID) {
+        Intent intent = new Intent(MainActivity.this, ContactDetailsActivity.class);
+        intent.putExtra(ContactDetailsActivity.CONTACT_ID, contactID);
+        startActivity(intent);
+    }
+
+
+    private void showAlert(String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message);
+        builder.setPositiveButton("Agree", null);
+        builder.show();
     }
 
 }
