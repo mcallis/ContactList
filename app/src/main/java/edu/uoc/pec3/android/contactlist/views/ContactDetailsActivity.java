80df610542ed121619f60286a00f4bf8722abaff
Marc Callís Vilalta <callis.marc@gmail.com>
2016-05-12 20:08:58 +0200

    Show info in Detail Activity

--- app/src/main/java/edu/uoc/pec3/android/contactlist/views/ContactDetailsActivity.java
+++ app/src/main/java/edu/uoc/pec3/android/contactlist/views/ContactDetailsActivity.java
@@ -1,17 +1,37 @@
 package edu.uoc.pec3.android.contactlist.views;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import com.pkmmte.view.CircularImageView;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 import edu.uoc.pec3.android.contactlist.R;
+import edu.uoc.pec3.android.contactlist.manager.FirebaseContactManager;
+import edu.uoc.pec3.android.contactlist.model.Contact;
+import edu.uoc.pec3.android.contactlist.utils.Alert;
 
 public class ContactDetailsActivity extends AppCompatActivity {
 
     public static final String CONTACT_ID = "ContactId";
+
+    private CircularImageView mImage;
+    private TextView mNameTxt;
+    private TextView mPhoneTxt;
+    private TextView mEmailTxt;
+    private TextView mCityTxt;
+    private TextView mDescTxt;
+    private Contact mCurrentContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +40,42 @@ public class ContactDetailsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setHomeAsUpIndicator(getArrow());
+
+        mImage = (CircularImageView)findViewById(R.id.circular_image);
+        mNameTxt = (TextView)findViewById(R.id.name);
+        mPhoneTxt = (TextView)findViewById(R.id.phone);
+        mEmailTxt = (TextView)findViewById(R.id.mail);
+        mCityTxt = (TextView)findViewById(R.id.city);
+        mDescTxt = (TextView)findViewById(R.id.description);
+
+
+        mCurrentContact = FirebaseContactManager.getInstance().getContactByObjectId(getIntent().getStringExtra(CONTACT_ID));
+
+        if (mCurrentContact == null){
+            Alert.show(this, "Contact information is not available");
+        } else {
+            showDetails();
+        }
     }
+
+    private Drawable getArrow() {
+        final Drawable upArrow = ContextCompat.getDrawable(this, R.drawable.abc_ic_ab_back_material);
+        upArrow.setColorFilter(ContextCompat.getColor(this, android.R.color.white), PorterDuff.Mode.SRC_ATOP);
+        return upArrow;
+    }
+
+    private void showDetails() {
+        Picasso.with(this).load(mCurrentContact.getImageUrl()).into(mImage);
+        mNameTxt.setText(mCurrentContact.getName());
+        mPhoneTxt.setText(mCurrentContact.getPhone());
+        mEmailTxt.setText(mCurrentContact.getEmail());
+        mCityTxt.setText(mCurrentContact.getCity());
+        mDescTxt.setText(mCurrentContact.getDescription());
+    }
+
+
 
 }
